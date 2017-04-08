6ec7299aa5e4ac56364a1b366d0f534fcbd69c9b
vtcurry <vtcurry@gmail.com>
2017-04-08 14:28:16 -0400

    Finish logout route

--- src/main/java/com/theironyard/novauc/AngularSpringController.java
+++ src/main/java/com/theironyard/novauc/AngularSpringController.java
@@ -1,8 +1,13 @@
 package com.theironyard.novauc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 /**
@@ -48,11 +53,14 @@ public class AngularSpringController {
 
 
     }
-    @RequestMapping(path = "/logout/", method = RequestMethod.POST)
-    public String logout(){
-
-        return"";
-
+    @RequestMapping(path="/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        request.setAttribute("logout","logout");
+        return "login";
     }
 }
 
